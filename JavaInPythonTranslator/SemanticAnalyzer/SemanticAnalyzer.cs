f4349773decb1592146c3198cf89f5f849a3775f
Анатолий Скопецкий <P0wderGang3r@outlook.com>
2021-12-10 23:10:07 +1000

    Внесение правки в название

--- JavaInPythonTranslator/SemanticAnalyzer/SemanticAnalyzer.cs
+++ JavaInPythonTranslator/SemanticAnalyzer/SemanticAnalyzer.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JavaInPythonTranslator
+namespace JavaInPythonTranslator.SemanticAnalyzer
 {
-    internal class MorphologicalAnalyzer
+    internal class SemanticAnalyzer
     {
     }
 }
